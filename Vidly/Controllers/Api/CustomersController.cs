a948678e81ab1b4a86fee1f431483d07e2c7e1e3
JoaoFerreira10 <joao.ferreira10@hotmail.com>
2016-10-10 16:20:30 +0100

    Added Customers API

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+
+        public ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        [HttpGet]
+        public IHttpActionResult GetCustomers()
+        {
+            var customers = _context.Customers.Include(m => m.MemberShipType).ToList();
+
+            return Ok(customers);
+
+        }
+    }
+}
